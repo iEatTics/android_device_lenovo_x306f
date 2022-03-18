824d219e0edd27413cb01585e2c30d53fada1a91
iEatTics <aaronrector09@gmail.com>
2022-03-18 14:13:29 -0500

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_X306F.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_X306F-user \
+    omni_X306F-userdebug \
+    omni_X306F-eng
